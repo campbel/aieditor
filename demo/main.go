32d3088fe7186392ce2e6826e677055e6fb49bb4
Chris Campbell <chrs.campbell@shopify.com>
2023-03-16 17:17:54 -0600

    spent way to much time on this today

--- demo/main.go
+++ demo/main.go
@@ -1,37 +1,18 @@
 package main
 
 import (
-    "net/http"
-    "math"
-    "strconv"
-    "github.com/charmbracelet/log"
+	"fmt"
+	"net/http"
+
+	"github.com/charmbracelet/log"
 )
 
 func main() {
-    log.Println("Starting...")
-    http.HandleFunc("/", handler)
-    http.HandleFunc("/prime", primeHandler)
-    err := http.ListenAndServe(":8080", nil)
-    if err != nil {
-        log.Println(err)
-    }
+	log.Info("starting...")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintln(w, "Hello World")
-}
-
-func primeHandler(w http.ResponseWriter, r *http.Request) {
-    q := r.URL.Query().Get("number")
-    num, err := strconv.Atoi(q)
-    if err != nil {
-        log.Println(err)
-    }
-    for i := 2; i < int(math.Sqrt(float64(num))+1); i++ {
-        if num % i == 0 {
-            fmt.Fprintln(w, "Number is not prime")
-            return
-        }
-    }
-    fmt.Fprintln(w, "Number is prime")
+	fmt.Fprintln(w, "Hello, world")
 }
--- go.mod
+++ go.mod
@@ -11,8 +11,10 @@ require (
 	github.com/charmbracelet/bubbles v0.15.0 // indirect
 	github.com/charmbracelet/bubbletea v0.23.1 // indirect
 	github.com/charmbracelet/lipgloss v0.7.1 // indirect
+	github.com/charmbracelet/log v0.1.2 // indirect
 	github.com/containerd/console v1.0.3 // indirect
 	github.com/dlclark/regexp2 v1.4.0 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
